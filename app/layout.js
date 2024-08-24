19a343ae49093e0f10866d9900432cdbeb422dfa
Benedict <chensxb97@gmail.com>
2024-08-24 11:12:34 +0800

    updated metadata object in layout

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,29 @@
+export const metadata = {
+    title: 'React App',
+    description: 'Web site created with Next.js.',
+    icons: [
+        { rel: 'icon1', url: '/logo192.png' },
+        { rel: 'icon2', url: '/logo512.png' },
+    ],
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    }
+}
+
 export default function RootLayout({ children }) {
-    return '...'
+    return (
+        <html lang="en">
+            <body>
+                <div id="root">{children}</div>
+            </body>
+        </html>
+    )
 }
